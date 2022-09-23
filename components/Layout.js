445b545aba2188379fdb024f18f3254ae283cd56
olaoluwa vincent <olaoluwavincent@gmail.com>
2022-09-23 04:32:00 +0100

    Init: Layout, Header and Footer Components

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import styles from '../styles/Layout.module.css';
+import Footer from './Footer';
+import Header from './Header';
+
+const Layout = ({ title, keywords, description, children }) => {
+	return (
+		<div>
+			<Head>
+				<title>{title}</title>
+				<meta name='description' content={description} />
+				<meta name='keywords' content={keywords} />
+			</Head>
+			<Header />
+			<div className={styles.container}>{children}</div>
+			<Footer />
+		</div>
+	);
+};
+
+export default Layout;
+
+Layout.DefaultProps = {
+	title: 'DJ Events | Find the hotest parties',
+	description: 'Find the latest and other musical events',
+	keywords: 'dj,music',
+};
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import Layout from '../components/Layout';
 
 const AboutPage = () => {
 	return (
-		<div>
+		<Layout title={'Abou DJ Events'}>
 			<h1>About</h1>
 			<p>This is an app to find the Latest Dj and other Musical Events</p>
 			<p>Version 1.0.0</p>
-		</div>
+		</Layout>
 	);
 };
 
